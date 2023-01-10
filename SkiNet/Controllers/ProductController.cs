4d92a8af212a8dc5d35eb519a12ee53da9619786
petarmirkovic <petar.mirkovic188@gmail.com>
2023-01-10 13:15:17 +0100

    Initial Commit

---
+++ SkiNet/Controllers/ProductController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Core.Entities;
+using Infrastructure.Data;
+
+namespace SkiNet.Controllers
+{
+    [ApiController]
+    [Route("api/{controller}")]
+    public class ProductController : Controller
+    {
+        private readonly StoreContext _context;
+
+        public ProductController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = _context.Products.ToListAsync();
+            return Ok(products);
+        }
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+    }
+}
