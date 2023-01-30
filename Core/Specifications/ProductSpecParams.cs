4de49cececc8afb468787b2393d0bde7093593ab
petarmirkovic <petar.mirkovic188@gmail.com>
2023-01-30 11:21:21 +0100

    Added navbar, connected to api, product and pagination interfaces added

--- Core/Specifications/ProductSpecParams.cs
+++ Core/Specifications/ProductSpecParams.cs
@@ -17,11 +17,11 @@ namespace Core.Specifications
         }
         public int? BrandId { get; set; }
         public int? TypeId { get; set; }
-        public string Sort { get; set; }
-        private string _search;
+        public string Sort { get; set; } = "priceAsc";
+        private string _search = "";
         public string Search 
         {
-            get=>_search;
+            get=> _search;
             set=> _search = value.ToLower();
         }
     }
